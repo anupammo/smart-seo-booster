07e700c28d63359bc540a377a34187c09d97bf7c
Anupam Mondal <a09051985@gmail.com>
2025-09-30 19:47:43 +0530

    Create faq-schema.php

---
+++ schema/faq-schema.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    "@context" => "https://schema.org",
+    "@type" => "FAQPage",
+    "mainEntity" => [
+        [
+            "@type" => "Question",
+            "name" => "What services does Anupam Mondal offer?",
+            "acceptedAnswer" => [
+                "@type" => "Answer",
+                "text" => "Web design, development, and SEO automation."
+            ]
+        ],
+        [
+            "@type" => "Question",
+            "name" => "Does Anupam provide schema automation?",
+            "acceptedAnswer" => [
+                "@type" => "Answer",
+                "text" => "Yes, including Article, FAQ, LocalBusiness, and more."
+            ]
+        ]
+    ]
+];

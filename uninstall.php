b2638f416ac7d71770944839280ae3f69c34d65b
Anupam Mondal <a09051985@gmail.com>
2025-09-30 20:43:33 +0530

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,9 @@
+<?php
+defined('WP_UNINSTALL_PLUGIN') || exit;
+
+// Delete plugin options
+delete_option('smart_seo_options');
+
+// Optional: delete post meta if you store schema type per post
+// global $wpdb;
+// $wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE meta_key = '_smart_seo_schema_type'");

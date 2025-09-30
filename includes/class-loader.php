322a0c13875d2ac7920d71b3b38ce8183cbb00c1
Anupam Mondal <a09051985@gmail.com>
2025-09-30 19:31:39 +0530

    Create class-loader.php

---
+++ includes/class-loader.php
@@ -0,0 +1,20 @@
+<?php
+defined('ABSPATH') || exit;
+
+class Smart_SEO_Loader {
+    public static function init() {
+        require_once plugin_dir_path(__FILE__) . 'class-settings.php';
+        require_once plugin_dir_path(__FILE__) . 'class-admin-ui.php';
+        require_once plugin_dir_path(__FILE__) . 'class-seo-core.php';
+        require_once plugin_dir_path(__FILE__) . 'class-schema-generator.php';
+        require_once plugin_dir_path(__FILE__) . 'class-content-auditor.php';
+        require_once plugin_dir_path(__FILE__) . 'class-link-analyzer.php';
+
+        Smart_SEO_Settings::init();
+        Smart_SEO_Admin_UI::init();
+        Smart_SEO_Core::init();
+        Smart_SEO_Schema_Generator::init();
+        Smart_SEO_Content_Auditor::init();
+        Smart_SEO_Link_Analyzer::init();
+    }
+}

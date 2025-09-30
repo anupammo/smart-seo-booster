c9101cd0ba873a2626772c8ef88ff9473e812b60
Anupam Mondal <a09051985@gmail.com>
2025-09-30 19:46:29 +0530

    Create article-schema.php

---
+++ schema/article-schema.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    "@context" => "https://schema.org",
+    "@type" => "Article",
+    "headline" => get_the_title(),
+    "author" => [
+        "@type" => "Person",
+        "name" => "Anupam Mondal"
+    ],
+    "datePublished" => get_the_date('c'),
+    "dateModified" => get_the_modified_date('c'),
+    "mainEntityOfPage" => [
+        "@type" => "WebPage",
+        "@id" => get_permalink()
+    ],
+    "publisher" => [
+        "@type" => "Organization",
+        "name" => get_bloginfo('name'),
+        "logo" => [
+            "@type" => "ImageObject",
+            "url" => get_site_icon_url()
+        ]
+    ]
+];

4a6478782134939637f071ec450b8dfe760967c4
Anupam Mondal <a09051985@gmail.com>
2025-09-30 19:48:06 +0530

    Create local-business-schema.php

---
+++ schema/local-business-schema.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    "@context" => "https://schema.org",
+    "@type" => "LocalBusiness",
+    "name" => "Anupam Mondal Web Solutions",
+    "image" => get_site_icon_url(),
+    "address" => [
+        "@type" => "PostalAddress",
+        "streetAddress" => "Metiari, Protapnagar, Sonarpur",
+        "addressLocality" => "South 24 Parganas",
+        "addressRegion" => "West Bengal",
+        "postalCode" => "700150",
+        "addressCountry" => "IN"
+    ],
+    "url" => home_url(),
+    "telephone" => "+91-XXXXXXXXXX"
+];
